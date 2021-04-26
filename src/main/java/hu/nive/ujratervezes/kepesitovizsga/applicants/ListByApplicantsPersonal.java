e5cb813d54e09bd08b0da46aec673f1ea13216ee
BirkasTivadar <birkastivadar@gmail.com>
2021-04-26 05:40:22 +0200

    Applicants kis javítás

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/applicants/ListByApplicantsPersonal.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/applicants/ListByApplicantsPersonal.java
@@ -1,10 +1,7 @@
 package hu.nive.ujratervezes.kepesitovizsga.applicants;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,20 +9,15 @@ public class ListByApplicantsPersonal implements ApplicantListGenerator {
 
     @Override
     public List<Applicant> getListFromDatabase(DataSource dataSource) {
+
         List<Applicant> result = new ArrayList<>();
+
         try (
                 Connection conn = dataSource.getConnection();
-                PreparedStatement ps = conn.prepareStatement("SELECT first_name, last_name, phone_number, email FROM applicants;")
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT first_name, last_name, phone_number, email FROM applicants;")
         ) {
-            loadResultList(result, ps);
-        } catch (SQLException sqlException) {
-            throw new IllegalStateException("Connection failed", sqlException);
-        }
-        return result;
-    }
 
-    private void loadResultList(List<Applicant> result, PreparedStatement ps) {
-        try (ResultSet rs = ps.executeQuery()) {
             while (rs.next()) {
                 String firstName = rs.getString("first_name");
                 String lastName = rs.getString("last_name");
@@ -33,8 +25,10 @@ public class ListByApplicantsPersonal implements ApplicantListGenerator {
                 String email = rs.getString("email");
                 result.add(new Applicant(firstName, lastName, phoneNumber, email));
             }
+
         } catch (SQLException sqlException) {
-            throw new IllegalStateException("Cannot query", sqlException);
+            throw new IllegalStateException("Connection failed", sqlException);
         }
+        return result;
     }
 }
--- src/test/java/hu/nive/ujratervezes/kepesitovizsga/applicants/ApplicantsTest.java
+++ src/test/java/hu/nive/ujratervezes/kepesitovizsga/applicants/ApplicantsTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicantsTest {
+
     private ApplicantListGenerator generator;
     private MysqlDataSource dataSource = new MysqlDataSource();
 
